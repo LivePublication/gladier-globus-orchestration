e329e3c363a1e61ff03d25bb1c84fe4fa69d1a12
Augustus Ellerm <ellermaugustus@gmail.com>
2024-03-18 12:59:59 +1300

    Adding better logging for the Globus-Gladier flow execution

--- flow.py
+++ flow.py
@@ -3,8 +3,11 @@ This file contains a single LivePublication Action Provider flow using Gladier &
 It serves as an experimental testbench for the LivePublication CLI.
 """
 from logger_config import logger
+from typing import Dict
 import json
+import time
 import os
+import datetime
 
 from gladier import GladierBaseClient, generate_flow_definition
 from globus_sdk.scopes import GCSCollectionScopeBuilder, TransferScopes
@@ -14,6 +17,24 @@ from pprint import pprint
 from config import DataStoreUUID, ComputeUUID, CollectionIDs, Compute_DS_UUID, InputPath, OutputPath, TransferFile
 from gladier_components.BeeMovieScript import GetBeeMovieScript
 from gladier_components.ListDirectory import FileSystemListCommand
+
+def extract_flow_info(status: Dict) -> str:
+    code = status['details'].get('code', 'Unknown')
+    description = status['details'].get('description', 'No description available')
+    state_name = status['details'].get('state_name', 'Unknown state')
+    action_status = status['details'].get('action_statuses', [])
+    action_info = ''
+    
+    if action_status:
+        action = action_status[0]  # Assuming we're interested in the first action
+        action_info = f"Action: {action.get('status', 'Unknown status')}, "
+        action_details = action.get('details', {})
+        action_info += f"Type: {action_details.get('type', 'Unknown type')}, "
+        action_info += f"Label: {action_details.get('label', 'No label')}, "
+        action_info += f"Status: {action_details.get('status', 'No status')}"
+    
+    log_entry = f"Code: {code}, Description: {description}, State: {state_name}, {action_info}"
+    return log_entry
 
 def run_flow():
     """
@@ -83,6 +104,15 @@ def run_flow():
 
     # action_id = flow['action_id']
     # example_flow_client.progress(action_id)
-    # pprint(flow.result(action_id))
+    # logger.info("Flow status:" + pprint(example_flow_client.get_status(action_id)))
+   
+    action_id = flow['action_id']
+    while True:
+        status = example_flow_client.get_status(action_id)
+        logger.info(extract_flow_info(status))
+        if status['status'] in ['SUCCEEDED', 'FAILED']:
+            break
+        # logger.info("Flow status:" + pprint(status)) 
+        time.sleep(5)
 
 run_flow()
